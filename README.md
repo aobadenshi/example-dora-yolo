fef260a1468f54514919e8253cb33133fe67ff4f
Shigeru Fujiwara <shigeru.fujiwara@aoba-denshi.com>
2026-01-09 03:37:05 +0900

    main.pyで実行するデモを追加

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 # このプロジェクトについて
 
 dora-rs の [Yolov8 の例](https://dora-rs.ai/docs/guides/getting-started/yolov8/) を簡単に試せるようにしたものである。
-GeForce GTX 1080 で動作するように依存ライブラリを設定している。
+GeForce GTX 1080 で動作するように依存ライブラリを設定しているので必要に応じて pyproject.toml を編集する。
 
 # Prerequisites
 
 - [uv](https://docs.astral.sh/uv/)
 - [dora](https://dora-rs.ai/)
 
-# 使い方
+# 使い方 (1)
 
 ```
 cd example-dora-yolo
@@ -23,4 +23,13 @@ dora start dataflow.yaml --uv
 dora destroy
 ```
 
+# 使い方 (2)
+
+```
+cd example-dora-yolo
+uv sync
+uv run main.py
+```
+
+終了は Ctrl-C
 
--- main.py
+++ main.py
@@ -1,6 +1,38 @@
-def main():
-    print("Hello from my-dora-yolo!")
+import subprocess
+import sys
+import time
+import os
 
+class DoraController:
+    def __init__(self, dataflow_yaml):
+        self.yaml_path = dataflow_yaml
+        self.process = None
+
+    def start(self):
+        if self.process is None:
+            subprocess.run(["dora", "up"], check=True)
+            self.process = subprocess.Popen(["dora", "start", self.yaml_path])
+            print(f"Dataflow {self.yaml_path} started (PID: {self.process.pid})")
+
+    def stop(self):
+        if self.process:
+            self.process.terminate()
+            self.process.wait()
+            print("Dataflow stopped.")
+            self.process = None
+        else:
+            subprocess.run(["dora", "stop"], check=False)
+
+def main():
+    try:
+        dc = DoraController('dataflow.yaml')
+        dc.start()
+        while True:
+            pass
+    except KeyboardInterrupt:
+        dc.stop()
+    finally:
+        sys.exit(0)
 
 if __name__ == "__main__":
     main()
